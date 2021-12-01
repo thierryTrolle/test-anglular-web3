40fcde187605544138fe24238552ab46145f5509
thierry <thierry.trolle@gmail.com>
2021-12-01 17:57:16 +0100

    import config web3 ok.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Web3 from 'web3';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testAnglularWeb3';
+  private web3: Web3;
+
+  constructor() {
+    this.web3 = new Web3("web3-provider-url");
+  }
 }
