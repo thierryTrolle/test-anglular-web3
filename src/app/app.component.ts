756c9248084423ca940f94ce7322294a90b1d653
thierry <thierry.trolle@gmail.com>
2021-12-03 00:58:14 +0100

    Web3 clean behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { Address } from 'cluster';
+import { EHOSTUNREACH } from 'constants';
 import Web3 from 'web3';
+
+declare let window: any;
 
 @Component({
   selector: 'app-root',
@@ -9,8 +13,46 @@ import Web3 from 'web3';
 export class AppComponent {
   title = 'testAnglularWeb3';
   private web3: Web3;
+  private currentAccount: string = '';
 
   constructor() {
-    this.web3 = new Web3("web3-provider-url");
+    // debugger
+    this.web3 = new Web3("http://localhost:7545");
+    if (typeof window.ethereum !== 'undefined') {
+      console.log('MetaMask is installed!');
+      // Use Mist/MetaMask's provider
+      window.ethereum.request({ method: 'eth_requestAccounts' }).then(() => {
+        console.log("Usi Mist/MetaMask's provider");
+        this.web3 = new Web3(window.ethereum);
+
+        window.ethereum.on("accountsChanged", (accounts: string | any[]) => {
+          if (accounts.length > 0) {
+            this.currentAccount = accounts[0];
+            console.log(`Account connected: ${accounts[0]}`);
+          } else {
+            console.log("Account disconnected");
+            window.location.reload();
+          }
+        });
+
+        //catch chainChanged
+        window.ethereum.on('chainChanged', (chainId: string) => {
+          console.log("chainChanged" + chainId);
+        });
+        window.ethereum.on('connect', () => console.log("event connect"));
+
+        window.ethereum.on('message', () => console.log("event message"));
+
+        window.ethereum.on('disconnect', () => console.log("event disconnect"));
+
+        window.ethereum.on('accountsChanged', () => console.log("event accountsChanged"));
+      }).catch((err: any) => {
+        if (err.code === 4001) { // EIP 1193 userRejectedRequest error
+          console.log('Please connect to MetaMask.')
+        } else {
+          console.error(err)
+        }
+      });
+    }
   }
 }
